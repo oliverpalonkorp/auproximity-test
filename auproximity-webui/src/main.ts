96c165a9eb1d6159aa94eca9b43ee6d509e2b4a8
miniduikboot <5243971+miniduikboot@users.noreply.github.com>
2021-04-10 14:39:58 +0200

    Add sentry integration to frontend

--- auproximity-webui/src/main.ts
+++ auproximity-webui/src/main.ts
@@ -1,4 +1,6 @@
 import Vue from 'vue'
+import * as Sentry from '@sentry/browser'
+import * as Integrations from '@sentry/integrations'
 import App from './App.vue'
 import router from './router'
 import store from './store'
@@ -7,6 +9,16 @@ import VueSocketIOExt from 'vue-socket.io-extended'
 import { io } from 'socket.io-client'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import consts from '@/consts'
+
+if (typeof process.env.VUE_APP_SENTRY_DSN !== 'undefined') {
+  console.log('Enabling automatic and anonymous error reporting')
+  Sentry.init({
+    dsn: process.env.VUE_APP_SENTRY_DSN,
+    integrations: [new Integrations.Vue({ Vue, attachProps: true })]
+  })
+} else {
+  console.log('Not enabling automatic error reporting')
+}
 
 Vue.config.devtools = true
 Vue.config.productionTip = false
